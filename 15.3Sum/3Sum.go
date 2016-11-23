5f9c7bbffd8e0ffb6590537c243e41c08a656f5a
chenshaobo <chenshaobo65@gmail.com>
2016-11-23 17:36:09 +0800

    add 15.3sum

---
+++ 15.3Sum/3Sum.go
@@ -0,0 +1,17 @@
+package threeSum
+
+
+func threeSum(nums []int) [][]int{
+	var result [][]int
+
+	length := len(nums)
+	if length == 0 {
+		return result
+	}
+
+	i:=0
+	for  ;i < length;i++{
+
+	}
+
+}
